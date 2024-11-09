1fcbcc5ea9b21b7de6c35fb9a81529cf5c0d8163
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-11-09 22:10:48 +0800

    Added AVL

---
+++ CS201 Project/AVL-Tree/src/main/java/edu/smu/smusql/AVLTree.java
@@ -0,0 +1,23 @@
+public class AVLTree {
+
+    private class Node {
+        int height;
+        Node left;
+        Node right;
+        String value;
+    }
+
+    private Node root;
+
+    public AVLTree() {
+        root = null;
+    }
+
+    public void insertNode(Node node) {
+        // if (root.value < node.)
+    }
+
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+    }
+}
