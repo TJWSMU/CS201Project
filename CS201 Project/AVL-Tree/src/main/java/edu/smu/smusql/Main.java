5b8e5e832e6546ccad69332ac04648c223609818
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-11-16 23:42:29 +0800

    a

--- CS201 Project/AVL-Tree/src/main/java/edu/smu/smusql/Main.java
+++ CS201 Project/AVL-Tree/src/main/java/edu/smu/smusql/Main.java
@@ -208,12 +208,12 @@ public class Main {
     
     public class DatabaseAutoEvaluator {
 
-        private static final int NUMBER_OF_QUERIES = 10000000;
+        private static final int NUMBER_OF_QUERIES = 80000;
         private static final String[] QUERY_TYPES = {"INSERT", "SELECT", "UPDATE", "DELETE"};
         private static final int QUERIES_PER_TYPE = NUMBER_OF_QUERIES / QUERY_TYPES.length;
         private static final Random RANDOM = new Random();
 
-        private static final boolean TEST_WITH_COMPLEX = false;
+        private static final boolean TEST_WITH_COMPLEX = true;
 
         // Counters to track the number of rows in each table
         private static int userCount = 50;
